df3261a43211cfc5669caee368dfd80078ef5f30
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-06-28 15:19:55 +0200

    added passport

---
+++ dcd/passport-dcd/passport-dcd.ts
@@ -0,0 +1,15 @@
+/**
+ * Module dependencies.
+ */
+import * as Strategy from './strategy';
+
+
+/**
+ * Expose `Strategy` directly from package.
+ */
+exports = module.exports = Strategy;
+
+/**
+ * Export constructors.
+ */
+exports.Strategy = Strategy;
