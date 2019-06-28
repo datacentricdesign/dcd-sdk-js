77126d528825caeb7a1d5ea37257750d27c3cebf
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-06-28 16:47:26 +0200

    tuning sdk

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 
 export { Person } from './entities/person'
 export { Property } from './entities/property'
+export { PropertyType } from './entities/property'
 export { Thing } from './entities/thing'
 
 export {GETRequest, POSTRequest, PUTRequest} from './helpers/http'
 
-export {Strategy} from './passport-dcd/strategy'
+export {PassportStrategy} from './passport-strategy'
