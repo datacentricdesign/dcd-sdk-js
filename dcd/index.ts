41b2a6baa2693c022af309b32d6d24392d1346f4
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-06-28 15:34:31 +0200

    added index.js

---
+++ dcd/index.ts
@@ -0,0 +1,8 @@
+
+export { Person } from './entities/person'
+export { Property } from './entities/property'
+export { Thing } from './entities/thing'
+
+export {GETRequest, POSTRequest, PUTRequest} from './helpers/http'
+
+export {Strategy} from './passport-dcd/strategy'
