c405fc14b2395bcc737057640de0f2e3bed93b71
issarompion <irompion@yahoo.fr>
2019-08-22 16:21:11 +0200

    cleaning

--- src/services/person_service.ts
+++ src/services/person_service.ts
@@ -8,33 +8,33 @@ export class PersonService {
 
     /**
      * function read user credentials.
-     * @param person_token 
+     * @param token 
      * @returns {Promise<any>}
      */
-    static async readUser(person_token:string):Promise<any>{
-        return http.GETRequest(user_url,person_token)
+    static async readUser(token:string):Promise<any>{
+        return http.GETRequest(user_url,token)
     }
     /**
      * function read user id (subject).
      * @param person_id 
-     * @param person_token 
+     * @param token 
      * @returns {Promise<any>}
      */
-    static async readUserId(person_id,person_token):Promise<any>{
-        return http.GETRequest(api_url+'/persons/'+person_id,person_token)
+    static async readUserId(person_id,token):Promise<any>{
+        return http.GETRequest(api_url+'/persons/'+person_id,token)
     }
     /**
      * Revoke login
      * @param person_sub 
-     * @param person_token
+     * @param token
      * @returns {Promise<any>} 
      */
-    static async logout(person_sub,person_token):Promise<any>{
-        return http.DELETERequest(encodeURI(auth_url+'/sessions/login?subject='+person_sub),person_token)
+    static async logout(person_sub,token):Promise<any>{
+        return http.DELETERequest(encodeURI(auth_url+'/sessions/login?subject='+person_sub),token)
     }
     /*
-    static async revokeConsent(person_sub,person_token):Promise<any>{
-        return http.DELETERequest(auth_url+'/sessions/consent?subject='+person_sub,person_token)
+    static async revokeConsent(person_sub,token):Promise<any>{
+        return http.DELETERequest(auth_url+'/sessions/consent?subject='+person_sub,token)
     }*/
 
 }
